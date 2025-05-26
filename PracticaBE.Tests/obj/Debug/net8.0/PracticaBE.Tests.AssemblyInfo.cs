cd3d92e008add9d206f6619e7576884938da1cc2
Will420M <william.e.ortiz.mendez@gmail.com>
2025-05-25 22:07:59 -0400

    Terminado!!

--- PracticaBE.Tests/obj/Debug/net8.0/PracticaBE.Tests.AssemblyInfo.cs
+++ PracticaBE.Tests/obj/Debug/net8.0/PracticaBE.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaBE.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de381b897030c0b4518445c245775a6dc6330f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ebc5a8aa23abf08802dc482c24bccc63ea9fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaBE.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaBE.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
